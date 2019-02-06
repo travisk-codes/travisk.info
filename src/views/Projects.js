858d7d1811ba371cf5a95fa57eafc02d479fb482
Travis Kohlbeck <me+git@travisk.info>
2019-02-06 18:04:57 -0500

    adds random notes project image and copy

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -6,13 +6,16 @@ import Page from '../components/Page'
 const datum_description = `
 A personal metrics management platform. Currently features easy tag-value entry, tag name auto-complete, and entry edit & deletion
 `
+const random_notes_description = `
+Presents a random music note on a regular beat. Features an adjustable tempo, optional note accidentals, and audio cue
+`
 
 export default props => (
   <Page palette={props.palette}>
     <Project title='Datum (web app)' style={{maxWidth: 'unset', width: '18em'}} img='datum.jpg' description={datum_description} />
     <Project title='Datum (CLI)' img='cat-pics/2.jpg' description={datum_description} />
     <Project title={`"Bag 'n Fits"`} img='cat-pics/2.jpg' description={datum_description} />
-    <Project title='React-Random-Notes' img='cat-pics/3.jpg' description={datum_description} />
+    <Project title='Random Notes' img='react-random-notes.png' description={random_notes_description} />
     <h1>Webpages</h1>
     <br />
     <Project title='Math Tutoring' img='cat-pics/4.jpg' description={datum_description} />
